6eb7ede95b1e802593ae1fe7ac7e7a94b3624d54
principal <principal@admin>
2016-02-01 23:39:43 -0500

    primera modificacion de archivos de la diviciones

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+/**
+ *
+ * @author principal
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 55;
+     int b = 25;
+     int c = 10;
+     int d = 10;
+      System.out.println("b % a = " + (b % a) );
+     System.out.println("c % a = " + (c % a) );
+    }
+    
+}
+    
+    
+
